bc75a0f4cc1e6187a95a158c5e83e2f2fd375688
JuKahl <julian.kahl@hotmail.de>
2021-02-13 19:06:48 +0100

    🚧 First result 🚧

---
+++ src/components/tcg/tcg.ts
@@ -0,0 +1,14 @@
+import { createElement } from "../../utils/createElement";
+import { pokemon } from "../../utils/api";
+import { APIPKM } from "../../utils/api";
+
+export function createCard({
+  data: { name, hp, types, evolvesFrom, images },
+}: pokemon) {
+  //   return createElement("div", {
+  //     innerText: name,
+  //   });
+  return createElement("img", {
+    src: images.small,
+  });
+}
