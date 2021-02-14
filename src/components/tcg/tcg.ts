9157b06b06031fabcc505ab0815ef31d41988c7c
JuKahl <julian.kahl@hotmail.de>
2021-02-14 21:24:05 +0100

    ✨ Add flickable card

--- src/components/tcg/tcg.ts
+++ src/components/tcg/tcg.ts
@@ -4,10 +4,42 @@ import { pokemon } from "../../utils/api";
 export function createCard({
   data: { name, hp, types, evolvesFrom, images },
 }: pokemon) {
-  //   return createElement("div", {
-  //     innerText: name,
-  //   });
-  return createElement("img", {
-    src: images.small,
+  return createElement("div", {
+    className: "card",
+    childs: [
+      createElement("div", {
+        className: "card__inner",
+        childs: [
+          createElement("img", {
+            className: "card__front",
+            src: images.small,
+          }),
+          createElement("div", {
+            className: "card__back",
+            childs: [
+              createElement("p", {
+                innerText: name,
+              }),
+              createElement("p", {
+                innerText: evolvesFrom,
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   });
 }
+//   return createElement("div", {
+//     className: "card",
+//     childs: [
+//       createElement("img", {
+//         src: images.small,
+
+//       }),
+//       createElement("p", {
+//         innerText: name,
+//       }),
+//     ],
+//   });
+// }
